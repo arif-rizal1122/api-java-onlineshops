04fa218c03c5cb496975eec4d53e8622366d8c7c
arif-rizal1122 <rizallarif32@gmail.com>
2024-12-29 15:00:39 +0800

    relationship between tables

--- src/main/java/com/arifRizal/online_shops/model/Category.java
+++ src/main/java/com/arifRizal/online_shops/model/Category.java
@@ -1,11 +1,25 @@
 package com.arifRizal.online_shops.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Category {
-    private  long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
     private String name;
 
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
 
 }
--- src/main/java/com/arifRizal/online_shops/model/Image.java
+++ src/main/java/com/arifRizal/online_shops/model/Image.java
@@ -1,14 +1,26 @@
 package com.arifRizal.online_shops.model;
 
-import java.sql.Blob;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.sql.Blob;
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
     private String fileName;
     private String fileType;
+    @Lob
     private Blob image;
     private String downloadUrl;
 
+    @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
-
 }
