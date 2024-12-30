64ba41c7953dfbf5ec40906f2b42f4ec9895f388
arif-rizal1122 <rizallarif32@gmail.com>
2024-12-30 21:09:47 +0800

    image implementasi Dto for save Image

---
+++ src/main/java/com/arifRizal/online_shops/dto/ImageDto.java
@@ -0,0 +1,13 @@
+package com.arifRizal.online_shops.dto;
+
+import lombok.Data;
+
+@Data
+public class ImageDto {
+
+    private  Long imageId;
+    private String imageName;
+    private String downloadUrl;
+
+
+}
--- src/main/java/com/arifRizal/online_shops/service/image/ImageService.java
+++ src/main/java/com/arifRizal/online_shops/service/image/ImageService.java
@@ -1,8 +1,11 @@
 package com.arifRizal.online_shops.service.image;
 
+import com.arifRizal.online_shops.dto.ImageDto;
 import com.arifRizal.online_shops.exception.ResourceNotFound;
 import com.arifRizal.online_shops.model.Image;
+import com.arifRizal.online_shops.model.Product;
 import com.arifRizal.online_shops.repository.ImageRepository;
+import com.arifRizal.online_shops.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,11 +13,14 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.sql.rowset.serial.SerialBlob;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ImageService implements IMageService{
     private final ImageRepository imageRepository;
+    private ProductService productService;
 
     @Override
     public Image getImageById(Long id) {
@@ -29,8 +35,36 @@ public class ImageService implements IMageService{
     }
 
     @Override
-    public Image saveImage(MultipartFile file, Long productId) {
-        return null;
+    public List<ImageDto> saveImages(List<MultipartFile>files, Long productId) {
+        Product product = productService.getProductById(productId);
+        List<ImageDto> savedImageDtos = new ArrayList<>();
+        for (MultipartFile file : files){
+            try {
+                Image image = new Image();
+                image.setFileName(file.getOriginalFilename());
+                image.setFileType(file.getContentType());
+                image.setImage(new SerialBlob(file.getBytes()));
+                image.setProduct(product);
+
+                String buildDownloadUrl = "/api/v1/images/image/download";
+                String downloadUrl = buildDownloadUrl +image.getId();
+                image.setDownloadUrl(downloadUrl);
+                Image savedImage = imageRepository.save(image);
+
+                savedImage.setDownloadUrl(buildDownloadUrl +image.getId());
+                imageRepository.save(savedImage);
+
+                ImageDto imageDto = new ImageDto();
+                imageDto.setImageId(savedImage.getId());
+                imageDto.setImageName(savedImage.getFileName());
+                imageDto.setDownloadUrl(savedImage.getDownloadUrl());
+                savedImageDtos.add(imageDto);
+
+            } catch (IOException | SQLException e){
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        return savedImageDtos;
     }
 
     @Override
