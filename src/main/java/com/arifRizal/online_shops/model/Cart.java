895caa85f625ae847b5dc2f1614337fad6f6368e
arif-rizal1122 <rizallarif32@gmail.com>
2025-02-03 22:41:30 +0800

    add entity cart & cartItem

---
+++ src/main/java/com/arifRizal/online_shops/model/Cart.java
@@ -0,0 +1,31 @@
+package com.arifRizal.online_shops.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class Cart {
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long id;
+     private BigDecimal totalAmount = BigDecimal.ZERO;
+
+     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+     private Set<CartItem> cartItems;
+
+
+
+}
